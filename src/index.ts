7547ddca90d9fd74a879c93346c5bd45d04d9ca2
Denis Carriere <carriere.denis@gmail.com>
2023-05-17 12:54:31 -0400

    add cli example build

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,30 @@ import { createAuthInterceptor, createRegistry, createRequest, streamBlocks, unp
 const SUBSTREAM = "https://github.com/streamingfast/substreams-uniswap-v3/releases/download/v0.2.1/substreams.spkg";
 const MODULE = "map_pools_created";
 
-const substream = await fetchSubstream(SUBSTREAM);
-const registry = createRegistry(substream);
-const transport = createGrpcTransport({
-  baseUrl: "https://api.streamingfast.io",
-  httpVersion: "2",
-  interceptors: [createAuthInterceptor(token)],
-  jsonOptions: {
-    typeRegistry: registry,
-  },
-});
+(async () => {
+  const substream = await fetchSubstream(SUBSTREAM);
+  const registry = createRegistry(substream);
+  const transport = createGrpcTransport({
+    baseUrl: "https://api.streamingfast.io",
+    httpVersion: "2",
+    interceptors: [createAuthInterceptor(token)],
+    jsonOptions: {
+      typeRegistry: registry,
+    },
+  });
 
-const request = createRequest({
-  substreamPackage: substream,
-  outputModule: MODULE,
-  productionMode: true,
-  startBlockNum: 17250000n,
-  stopBlockNum: 17260000n,
-});
+  const request = createRequest({
+    substreamPackage: substream,
+    outputModule: MODULE,
+    productionMode: true,
+    startBlockNum: 17250000n,
+    stopBlockNum: 17260000n,
+  });
 
-for await (const item of streamBlocks(transport, request)) {
-  const message = unpackMapOutput(item.response, registry);
-  if (message !== undefined) {
-    console.dir(message);
+  for await (const item of streamBlocks(transport, request)) {
+    const message = unpackMapOutput(item.response, registry);
+    if (message !== undefined) {
+      console.dir(message);
+    }
   }
-}
+})();
