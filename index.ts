4079b75b5fc702783e6eb10d8dbfd76b6a8f52ce
Denis Carriere <carriere.denis@gmail.com>
2023-07-12 18:03:06 -0400

    refactor: remove file sync, manifest handles that

--- index.ts
+++ index.ts
@@ -1,3 +1,2 @@
 export { BlockEmitter } from "./src/BlockEmitter.js";
 export { createDefaultTransport } from "./src/createDefaultTransport.js";
-export { readFileSyncSubstream } from "./src/readFileSyncSubstream.js";
