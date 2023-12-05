f1009a842da258de9162e8762da3dfddb0b3f0ab
Denis Carriere <carriere.denis@gmail.com>
2023-12-05 12:43:10 -0500

    export `createWebTransport`

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export { BlockEmitter } from "./src/BlockEmitter.js";
 export { createDefaultTransport } from "./src/createDefaultTransport.js";
+export { createNodeTransport } from "./src/createNodeTransport.js";
+export { createWebTransport } from "./src/createWebTransport.js";
 export { createHeadersInterceptor } from "./src/createHeadersInterceptor.js";
